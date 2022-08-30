dde30ecc3853337e384dd5a5d36279bb801c1bc1
Wiktor Morawski <wiktor.morawski2000@onet.pl>
2022-08-30 14:09:12 +0200

    Added ComponentB

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import ComponentC from "./Test_repo_submodule/component";
+import ComponentB from "./Test_repo_submodule/ComponentB";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <ComponentC />
+        <ComponentB />
       </header>
     </div>
   );
